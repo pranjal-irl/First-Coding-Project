5935e5ec1be2f698851d22bcea3ad6e23947b428
Pranjal <pranjal.quantom@gmail.com>
2026-05-27 14:51:53 +0530

    fixing

--- Home.py
+++ Home.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.title("Hello!")
-st.text("I am learning streamlit which is a python framework")
-st.subheader("These are small projects/pages I made while learning different syntax")
-st.subheader("Just click on one of these or you can also access all the pages from the sidebar")
-
-st.page_link("pages/project1.py", title="Know your Favourite Programming Language", icon="1️⃣")
-st.page_link("pages/project2.py", title="Know about your Coding Level", icon="2️⃣")
+pg = st.navigation([
+    st.Page("pages/home.py", title="Home", icon="🏠", default=True),
+    st.Page("pages/project1.py", title="Know your Favourite Programming Language", icon="1️⃣"),
+    st.Page("pages/project2.py", title="Know about your Coding Level", icon="2️⃣"),
+])
+pg.run()
