6396590a7e84baa85910e5330ab2d97fc1a60f19
Pranjal <pranjal.quantom@gmail.com>
2026-05-27 14:53:33 +0530

    fixing

--- pages/home.py
+++ pages/home.py
@@ -5,5 +5,5 @@ st.text("I am learning streamlit which is a python framework")
 st.subheader("These are small projects/pages I made while learning different syntax")
 st.subheader("Just click on one of these or you can also access all the pages from the sidebar")
 
-st.page_link("pages/project1.py", title="Know your Favourite Programming Language", icon="1️⃣")
-st.page_link("pages/project2.py", title="Know about your Coding Level", icon="2️⃣")
+st.page_link("pages/project1.py", label="Know your Favourite Programming Language", icon="1️⃣")
+st.page_link("pages/project2.py", label="Know about your Coding Level", icon="2️⃣")
