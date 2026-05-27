db21a4e72837814886ce3d857163d8fd4587b721
Pranjal <pranjal.quantom@gmail.com>
2026-05-27 14:14:01 +0530

    fixing icon error

--- Home.py
+++ Home.py
@@ -4,5 +4,6 @@ st.title("Pages")
 st.subheader("I am learning streamlit which is a python framework")
 st.text("These are small projects/pages I made while learning different syntax")
 st.page_link("Home.py", label="Home", icon="🏠")
-st.page_link("project1.py", label="Know your Favourite Programming Language", icon="1️⃣")
-st.page_link("project2.py", label="Know your Coding Level", icon="2️⃣")
+st.page_link("pages/project1.py", label="Know your Favourite Programming Language", icon="1️⃣")
+st.page_link("pages/project2.py", label="Know your Coding Level", icon="2️⃣")
+
