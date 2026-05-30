b46c4cb5301c6afc9a3e2984d61501d30f4178bf
Pranjal <pranjal.quantom@gmail.com>
2026-05-30 13:16:11 +0530

    edited question

--- pages/project2.py
+++ pages/project2.py
@@ -24,13 +24,23 @@ set_bg_image()
 
 
 
-st.title("Know about your coding level")
+st.title("WHAT'S YOUR CODING LEVEL?")
 st.text("You probably know where you are but have you ever thought in this way?")
 
 st.text("Starting with the basic question")
-phase = st.selectbox("Which phase of life are you in while coding?", ["Choose phase", "School 🙋‍♂️","In Between School and College period 👻" "College 🧑‍🎓", "Doing internship 😋", "Doing a Job 🧑‍💻", "Jobless at Home 😭🙏"])
-if phase != "Choose phase":
-    st.write("Alright...Good Luck for the Future gang!")
+phase = st.selectbox("Which phase of life are you in while coding?", ["Choose phase", "School 🙋‍♂️","In Between School and College period 👻", "College 🧑‍🎓", "Doing internship 😋", "Doing a job 🧑‍💻", "Jobless at Home 😭🙏"])
+if phase == "School 🙋‍♂️":
+    st.write("Starting at the right time gang!")
+if phase == "In Between School and College period 👻":
+    st.write("Us bro us")
+if phase == "College 🧑‍🎓":
+    st.write("Grind time twin!")
+if phase == "Doing internship 😋":
+    st.write("Amazinggg!")
+if phase == "Doing a job 🧑‍💻":
+    st.write("Alright techie!")
+if phase == "Jobless at Home 😭🙏":
+    st.write("Keep going! the work will definitely pay off!")
 
 time = st.radio("How long have you been coding?" , ["0-2 months" , "2-6 months" , "6 months - 1 year" , "1-2 years" , "2-5 years" , "5-10 years" , "10+ years"])
 if time == "0-2 months":
@@ -47,3 +57,4 @@ elif time == "5-10 years":
     st.write("You're killing it ngl! 🫶")
 elif time == "10+ years":
     st.write("A master of work 🫡")
+
